8891b42bb1c9c7b69ff398fe58143ed57d75551a
João Pereira <joao.pereira@cxpbrasil.com.br>
2026-04-15 12:21:32 -0300

    Add uma rota users e lista usuarios com um let

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,25 @@
-import express from "express";
+import express, { Request, Response } from "express";
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello Word");
-})
+app.get("/", (req: Request, res: Response) => {
+    res.send("Bem vindo ao Curso de Node.js");
+});
+
+app.get("/users", (req: Request, res: Response) => {
+    
+    let usuarios = [{
+        "nome": "João Vitor",
+        "idade": 21
+    },
+    {
+        "nome": "Otavio Pereira",
+        "ideade": 45 
+    }]
+
+
+    res.send(usuarios)
+});
 
 app.listen(3000, () => {
     console.log("Servidor ativo na porta 3000")
