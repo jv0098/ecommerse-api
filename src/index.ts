c50c41ea303c2088442800e366c8cd2a91d742b6
João Pereira <joao.pereira@cxpbrasil.com.br>
2026-04-16 19:48:59 -0300

    Metodos HTTP [GET, POST, PUT, DELETE]

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,65 @@
 import express, { Request, Response } from "express";
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Bem vindo ao Curso de Node.js");
 });
 
+
+type User = {
+    id: number;
+    nome: string;
+    email: string;
+};
+
+let id = 0;
+let usuarios: User[] = [];
+
 app.get("/users", (req: Request, res: Response) => {
-    
-    let usuarios = [{
-        "nome": "João Vitor",
-        "idade": 21
-    },
-    {
-        "nome": "Otavio Pereira",
-        "ideade": 45 
-    }]
-
-
     res.send(usuarios)
 });
+
+app.get("/users/:id", (req: Request, res: Response) => {
+    let userId = Number(req.params.id);
+    let user = usuarios.find(user => user.id === userId);
+    res.send(user);
+})
+
+app.post("/users", (req: Request, res: Response) => {
+    let user = req.body;
+    user.id = ++id;
+    usuarios.push(user);
+    res.send({
+        message: "Usuario criado com sucesso"
+    });
+});
+
+app.put("/users/:id", (req: Request, res: Response) => {
+    let userId = Number(req.params.id);
+    let user = req.body;
+    let indexOf = usuarios.findIndex((_user: User) => _user.id === userId);
+    usuarios[indexOf].nome = user.nome;
+    usuarios[indexOf].email = user.email;
+    res.send({
+        message: "Usuario alterado com sucesso"
+    });
+});
+
+app.delete("/users/:id", (req: Request, res: Response) => {
+    let userId = Number(req.params.id);
+    let indexOf = usuarios.findIndex((user: User) => user.id === userId);
+    usuarios.splice(indexOf, 1);
+    res.send({
+        message: "Usuario excluido com sucesos!"
+    })
+})
+
+
+
+
+
 
 app.listen(3000, () => {
     console.log("Servidor ativo na porta 3000")
